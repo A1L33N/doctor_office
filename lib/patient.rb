43605fb1b32d89ef2db4047d9b9c1696ba09dc52
Michael Bunker & Aileen Cacayorin <student@epicodus.com>
2015-08-18 15:33:54 -0700

    add #initialize method to Patient class

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,11 +1,11 @@
 class Patient
 
-attr_reader :name, :id, :birthday
+attr_reader :name, :id, :birthday, :doctor_id
 
   define_method(:initialize) do |attributes|
     @name = attributes.fetch(:name)
     @id = attributes.fetch(:id)
     @birthday = attributes.fetch(:birthday)
   end
-  
+
 end
